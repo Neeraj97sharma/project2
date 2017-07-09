2a2a3cf576314fd6b794902d35866ce9f7e9356c
Neeraj Sharma <sharmaneeraj.hp@gmail.com>
2017-07-09 13:33:37 +0530

    fetch recent post

--- instabot.py
+++ instabot.py
@@ -1,5 +1,7 @@
 #instabot.py is initialized
-
+from get_own_post import get_own_post
 from self_info import self_info
-self_info()
+#self_info()
 #fetching own informtion
+result = get_own_post()
+print result
