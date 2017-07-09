832770be78ef25f1e175f29d699fb8435ecb49fc
Neeraj Sharma <sharmaneeraj.hp@gmail.com>
2017-07-09 11:37:37 +0530

    global variables file initialized

---
+++ constants.py
@@ -0,0 +1,5 @@
+# it contained global variables
+#instagram access token
+APP_ACCESS_TOKEN= '#5693959673.a666e48.fc4d31d7c3ea4bc093a12cc16b6e04cd'
+#instagram API base url
+BASE_URL='https://api.instagram.com/v1/'
