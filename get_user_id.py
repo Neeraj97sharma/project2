60b70956113e62fa18a3dc554cb2e76f17b184fc
Neeraj Sharma <sharmaneeraj.hp@gmail.com>
2017-07-11 10:33:51 +0530

    all function of instabot worked successfully

--- get_user_id.py
+++ get_user_id.py
@@ -1,21 +1,15 @@
-import requests
-from constants import APP_ACCESS_TOKEN, BASE_URL
+import  requests
+from constants import BASE_URL ,APP_ACCESS_TOKEN
+
 def get_user_id(insta_username):
-    # getting freinds data
-    user_id = get_user_id(insta_username)
-    if user_id == None:
-        print 'User does not exist!'
-        exit()
-    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
-    user_info = requests.get(request_url).json()
-
-    if user_info['meta']['code'] == 200:
-        if len(user_info['data']):
-            return user_info['data'][0]['id']
-        else:
-            return None
-    else:
-        print"status code other than 200"
-        exit()
-
+  request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+  print 'GET request url : %s' %(request_url)
+  user_info=requests.get(request_url).json()
+  if user_info['meta']['code'] ==200:
+      if len(user_info['data']):
+          return user_info['data'][0]['id']
+      else:
+          print "user not exist"
+  else:
+      print "ACCESS DENIED"
+      exit()
--- instabot.py
+++ instabot.py
@@ -1,7 +1,55 @@
-#instabot.py is initialized
-from get_own_post import get_own_post
+#start your bot and made the option for every avalaiblity in this file
+from __future__ import print_function
 from self_info import self_info
-#self_info()
-#fetching own informtion
-get_own_post()
+from get_user_info import get_user_info
+from like_post import like_post
+from  get_user_post import get_user_post
+from post_comment import post_comment
+from delete_neg_comnt import del_neg_comment
+from get_own_post import get_own_post
+import time
+#def fuction for start instabot
+def init_bot():
+    #this list cotain users options
+    option_list = ['\t\t\t\t*************************Welcome to instabot*****************************',
+                   'A.Get your own details', 'B..Get details of user by username', 'C..get your own recent post',
+                   'D..get recent post of user by username ', 'E..Like most recent user post',
+                   'F..make comment on user recent post ', 'G..Delete negative comment from recent post']
+    #loop for print the option and time delay for each option is .200 microsecond
+    for x in range(0,len(option_list)):
+        print(option_list[x],end='\n\n')
+        time.sleep(.200)
+
+    #enter a choice
+    select_option =raw_input("Enter your choice::")
+    select_option=select_option.upper()
+    if select_option == 'A':
+        self_info()
+
+    elif select_option == 'B':
+        insta_username = raw_input("Enter username: ")
+        get_user_info(insta_username)
+
+    elif select_option == 'C':
+        get_own_post()
+
+    elif select_option == 'D':
+        insta_username = raw_input("Enter username::")
+        get_user_post(insta_username)
+
+    elif select_option == 'E':
+        insta_username = raw_input("Enter username::")
+        like_a_post(insta_username)
+
+    elif select_option == 'F':
+        insta_username = raw_input("Enter username::")
+        post_comment(insta_username)
+
+    elif select_option == 'G':
+        insta_username = raw_input("Enter Username::")
+        del_neg_comment(insta_username)
+
+#init_bot() is called
+init_bot()
+
 
